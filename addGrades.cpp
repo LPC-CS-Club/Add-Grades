87d5bfcd05ac2862e1bb0ec96b7e7f2f6da672a5
trainfan500 <cjlinke@zonemail.clpccd.edu>
2024-11-04 23:02:29 +0000

    Added code based on pseudocode

--- addGrades.cpp
+++ addGrades.cpp
@@ -1,3 +1,34 @@
 #include "addGrades.hpp"
 // This is the add grades function!
-// I'm just testing GitHub stuff, no code was written yet
+#include <iostream>
+
+using namespace std;
+
+struct student {
+	string studentID;
+	string assignment;
+	double grade;
+};
+
+void addGrade(string, string, double);
+
+int main() {
+
+	return 0;
+}
+
+/* Pseudeocode:
+ * Function: addGrade(string studentID, string assignment, double grade)
+ * 			1) Get studentID using getUserInput("Enter Student ID: ")
+ * 			2) Get task using getUserInput("Enter Assignment Name: ")
+ * 			3) Get grade using getUserInput("Enter Grade: ")
+ * 			4) Add the task and grade to the student's record
+ */
+
+void addGrade(string studentID, string assignment, double grade) {
+	student randomStudent;
+	randomStudent.studentID = getUserInput("Enter Student ID: ");
+	randomStudent.assignment = getUserInput("Enter Assignment Name: ");
+	randomStudent.grade = getUserInput("Enter Grade: ");
+
+}
